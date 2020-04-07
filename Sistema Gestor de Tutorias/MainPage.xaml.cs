8e5c3acff06b03a4b64389e1fd06147a53291192
Panchis7u7 <smadrigal.rod@gmail.com>
2020-04-07 16:24:19 -0500

    Arreglos de Interfaz

--- Sistema Gestor de Tutorias/MainPage.xaml.cs
+++ Sistema Gestor de Tutorias/MainPage.xaml.cs
@@ -5,6 +5,8 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Foundation.Metadata;
+using Windows.UI;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -27,13 +29,14 @@ namespace Sistema_Gestor_de_Tutorias
         {
             this.InitializeComponent();
             config_page = new Pagina_Configuracion(ref mainPageRef, ref navMenu);
+
         }
 
         private void navMenu_BackRequested(NavigationView sender, NavigationViewBackRequestedEventArgs args)
         {
-
+           
         }
-
+        
         private void navMenu_SelectionChanged(NavigationView sender, NavigationViewSelectionChangedEventArgs args)
         {
             if (args.IsSettingsSelected)
@@ -57,6 +60,26 @@ namespace Sistema_Gestor_de_Tutorias
                     default:
                         break;
                 }
+            }
+        }
+
+        private void navMenu_Loaded(object sender, RoutedEventArgs e)
+        {
+            var navView = sender as NavigationView;
+            var rootGrid = VisualTreeHelper.GetChild(navView, 0) as Grid;
+
+            // SDK 18362 (1903)
+            // SDK 17763 (1809)
+            if (ApiInformation.IsApiContractPresent("Windows.Foundation.UniversalApiContract", 7))
+            {
+                // Find the back button.
+                var paneToggleButtonGrid = VisualTreeHelper.GetChild(rootGrid, 0) as Grid;
+                var buttonHolderGrid = VisualTreeHelper.GetChild(paneToggleButtonGrid, 1) as Grid;
+                var navigationViewBackButton = VisualTreeHelper.GetChild(buttonHolderGrid, 0) as Button;
+                navigationViewBackButton.AccessKey = "A";
+                SolidColorBrush back_black = new SolidColorBrush();
+                back_black.Color = Color.FromArgb(255, 0, 0, 0);
+                navigationViewBackButton.Foreground = back_black;
             }
         }
     }
