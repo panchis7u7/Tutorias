5db98fb87cf59fd955082660a58a4e5414907c7f
Panchis7u7 <smadrigal.rod@gmail.com>
2020-04-07 15:13:07 -0500

    Reestructuracion del acceso a las diferentes interfaces

--- Sistema Gestor de Tutorias/MainPage.xaml.cs
+++ Sistema Gestor de Tutorias/MainPage.xaml.cs
@@ -23,30 +23,10 @@ namespace Sistema_Gestor_de_Tutorias
     public sealed partial class MainPage : Page
     {
         private Pagina_Configuracion config_page;
-        public event EventHandler<NavigationViewSettingsInvokedEventArgs> settings_invoked;
         public MainPage()
         {
-            settings_invoked += btn_settings_Invoked;
             this.InitializeComponent();
             config_page = new Pagina_Configuracion(ref mainPageRef, ref navMenu);
-        }
-
-        private void navMenu_ItemInvoked(NavigationView sender, NavigationViewItemInvokedEventArgs args)
-        {
-            if (args.IsSettingsInvoked)
-            {
-                settings_invoked(this, new NavigationViewSettingsInvokedEventArgs());
-            }
-        }
-
-        private void btn_settings_Invoked(object sender, NavigationViewSettingsInvokedEventArgs args)
-        {
-            main_frame.Content = config_page;
-        }
-
-        private void navMenu_Loaded(object sender, RoutedEventArgs e)
-        {
-
         }
 
         private void navMenu_BackRequested(NavigationView sender, NavigationViewBackRequestedEventArgs args)
@@ -54,19 +34,30 @@ namespace Sistema_Gestor_de_Tutorias
 
         }
 
-        private void btn_home_Tapped(object sender, TappedRoutedEventArgs e)
+        private void navMenu_SelectionChanged(NavigationView sender, NavigationViewSelectionChangedEventArgs args)
         {
-
-        }
-
-        private void btn_Consultas_Tapped(object sender, TappedRoutedEventArgs e)
-        {
-
-        }
-
-        private void btn_formatos_Tapped(object sender, TappedRoutedEventArgs e)
-        {
-
+            if (args.IsSettingsSelected)
+            {
+                main_frame.Content = config_page;
+            }
+            else
+            {
+                NavigationViewItem item = args.SelectedItem as NavigationViewItem;
+                switch (item.Tag.ToString())
+                {
+                    case "1":
+                        main_frame.Navigate(typeof(Pagina_Configuracion));
+                        break;
+                    case "2":
+                        main_frame.Navigate(typeof(Pagina_Configuracion));
+                        break;
+                    case "3":
+                        main_frame.Navigate(typeof(Pagina_Configuracion));
+                        break;
+                    default:
+                        break;
+                }
+            }
         }
     }
 }
